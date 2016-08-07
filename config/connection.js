d5eebae3949e0fe60bbf30ecbec6d5915d45a753
Mike Magnani <mikemagnani5@gmail.com>
2016-08-07 11:20:24 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,27 @@
 var mysql = require('mysql');
 
 
+var source = {
+localhost:{
+      port: 3306,
+      host: 'localhost',
+      user: 'root',
+      password: 'yankeesny2',
+      database: 'burgers_db'
+},
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'yankeesny2',
-  database: 'burgers_db'
-});
+jawsDB : {
+      port: 3306,
+      host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: 'obk9qral5gv9lwys',
+      password: 'rxktd5he3n99t5jt',
+      database: 'burgers_db'
+
+  }
+  
+}
+
+var connection = mysql.createConnection( source.jawsDB );
 
 connection.connect(function (err) {
   if (err) {
